3c30d7412dced69ff70591a22a13ed3673bf2ec7
DarkLightBegin23 <samseong0418@naver.com>
2025-05-11 08:36:04 +0900

    20250511 flight methods

--- classflight.py
+++ classflight.py
@@ -2,10 +2,14 @@ class Flight:
     def __init__(self, number):
         self._number = number
     
-    
     def number(self):
-        return self._number
+        return self._number  # 인스턴스 변수 반환
     
+    def updateNumber(self, num):
+        self._number = num
+        return self._number
+
 # 인스턴스 생성
 f = Flight('KE2349')
+f.updateNumber("KE3485")
 print(f.number())
